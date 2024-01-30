aecb67b8d3264c7a203b756a735604be5c064e1e
JayabrataBasu2 <133545838+JayabrataBasu@users.noreply.github.com>
2024-01-31 03:25:47 +0530

    cardcomponenetsandbugs

--- Components/Friend/Chat/Chat.jsx
+++ Components/Friend/Chat/Chat.jsx
@@ -1,9 +1,124 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import { userRouter } from "next/router";
 
 //INTERNAL IMPORT
 import Style from "./Chat.module.css";
-const Chat = ({functionName, readMessage, friendMessage, account, userName, Loading,currentUserName, currentUserAddress}) => {
-  return <div></div>;
+import images from "../../../assets";
+import { convertTime } from "../../../Utils/apiFeature";
+import { Loader } from "../../index";
+
+const Chat = ({
+  functionName,
+  readMessage,
+  friendMessage,
+  account,
+  userName,
+  Loading,
+  currentUserName,
+  currentUserAddress,
+}) => {
+  //Use State
+  const [message, setMessage] = useState("");
+  const [chat, setChatData] = useState({
+    name: "",
+    address: "",
+  });
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!router.isReady) return;
+    setChatData(router.query);
+  }, [router.isReady]);
+
+  //console.log(chatData.address, chatData.name);
+  //for mobile i guess
+  return (
+    <div className={Style.Chat}>
+      {currentUserName && currentUserAddress ? (
+        <div className={Style.Chat_user_info}>
+          <Image src={images.accountName} alt="image" width={70} height={70} />
+          <div className={Style.Chat_user_info_box}>
+            <h4>{currentUserName}</h4>
+            <p className={Style.show}>{currentUserAddress}</p>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+
+      <div className={Style.Chat_box_box}>
+        <div className={Style.Chat_box}>
+          <div className={Style.Chat_box_left}>
+            {friendMsg.map((eL, i) => (
+              <div>
+                {eL.sender == chatData.address ? (
+                  <div className={Style.Chat_box_left_title}>
+                    <Image
+                      src={images.accountName}
+                      alt="image"
+                      width={50}
+                      height={50}
+                    />
+                    <span>
+                      {chatData.name} {""}
+                      <small>Time:{convertTime(eL.timestamp)}</small>
+                    </span>
+                  </div>
+                ) : (
+                  <div className={Style.Chat_box_left_title}>
+                    <Image
+                      src={images.accountName}
+                      alt="image"
+                      width={50}
+                      height={50}
+                    />
+                    <span>
+                      {userName} {""}
+                      <small>Time:{convertTime(eL.timestamp)}</small>
+                    </span>
+                  </div>
+                )}
+                <p key={i + 1}>
+                  {eL.msg}
+                  {""}
+                  {""}
+                </p>
+              </div>
+            ))}
+          </div>
+        </div>
+        {currentUserName && currentUserAddress ? (
+          <div className={Style.Chat_box_send}>
+            <div className={Style.Chat_box_send_img}>
+              <Image src={images.smile} alt="smile" width={50} height={50} />
+              <input
+                type="text"
+                placeholder="type your message"
+                onChange={(e) => setMessage(e.target.value)}
+              />
+              <Image src={images.file} alt="file" width={50} height={50} />
+              {Loading == true ? (
+                <Loader />
+              ) : (
+                <Image
+                  src={images.send}
+                  alt="file"
+                  width={50}
+                  height={50}
+                  onClick={() =>
+                    functionName({ msg: message, address: chatData.address })
+                  }
+                />
+              )}
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
