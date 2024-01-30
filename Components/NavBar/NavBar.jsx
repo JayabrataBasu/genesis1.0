2b1f2e4c507f91721010f3754b29bb88271d0ba3
Dhruv Jha <omdhruvjha@gmail.com>
2024-01-31 05:13:03 +0530

    New changes

--- Components/NavBar/NavBar.jsx
+++ Components/NavBar/NavBar.jsx
@@ -7,7 +7,7 @@ import Style from "./NavBar.module.css";
 import { ChapAppContact } from "../../Context/ChatAppContext";
 import { Model, Error } from "../index";
 import images from "../../assets";
-import { connectWallet } from "@/Utils/apiFeatures";
+import { connectWallet } from "../../Utils/apiFeatures";
 
 
 const NavBar = () => {
